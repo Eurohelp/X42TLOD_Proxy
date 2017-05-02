7cb99b2a16b32a6d97cad6ee80c829b7802b1761
grozadilla <gontzal.rozadilla@gmail.com>
2017-05-02 09:39:10 +0200

    sparql

--- src/main/java/eus/euskadi/opendata/lod/servlet/SparqlServlet.java
+++ src/main/java/eus/euskadi/opendata/lod/servlet/SparqlServlet.java
@@ -1,7 +1,9 @@
 package eus.euskadi.opendata.lod.servlet;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -12,14 +14,21 @@ import javax.servlet.http.HttpServletResponse;
 public class SparqlServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1031422249396784970L;
+	
+	private Properties properties = null;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		/*if (req.getHeader("Accept").contains("text/html")){
+			
+		}else{
+			
+		}*/
 		
 		resp.setContentType("text/html");
 		
 		PrintWriter out = resp.getWriter();
-		out.print("Sparql servlet");
+		out.print(properties.getProperty("sparql.initial.message"));
 		out.flush();
 		out.close();
 	}
@@ -33,6 +42,16 @@ public class SparqlServlet extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
 		// TODO Auto-generated method stub
-		super.init();
+		
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream input = classLoader.getResourceAsStream("proxy.properties");
+		// ...
+		properties = new Properties();
+		try {
+			properties.load(input);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new ServletException(e);
+		}
 	}
 }
